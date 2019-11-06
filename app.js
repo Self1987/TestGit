9243023582c4e062a7a57ced2eafe759b5e767c7
MadhuKodanda <47397097+MadhuKodanda@users.noreply.github.com>
2019-11-06 21:56:58 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('./node_modules/express');
+var app = express();
+
+app.get('/hello', function(req, res){
+   res.send("Hello World!");
+});
+
+app.post('/hello', function(req, res){
+   res.send("You just called the post method at '/hello'!\n");
+});
+
+app.listen(3000);
